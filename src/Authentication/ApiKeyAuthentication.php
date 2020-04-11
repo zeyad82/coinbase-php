69e085c312de342d9f1edb9548fb65354f083c2d
zeyad82 <zeyad82@yahoo.com>
2020-04-11 20:00:51 +0300

    Update ApiKeyAuthentication.php

--- src/Authentication/ApiKeyAuthentication.php
+++ src/Authentication/ApiKeyAuthentication.php
@@ -68,7 +68,10 @@ class ApiKeyAuthentication implements Authentication
 
     protected function getTimestamp()
     {
-        return now()->timestamp;
+        return json_decode(file_get_contents('https://api.coinbase.com/v2/time', false, stream_context_create(['ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false
+        ]])))->data->epoch;
     }
 
     protected function getHash($algo, $data, $key)
